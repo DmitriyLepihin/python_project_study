38a1067bddab4a1e0a3b3f3a44530f956f27ee4b
DmitriyLepihin <dmitrijlepihin1991@gmail.com>
2020-11-09 18:45:45 +0300

    Work hard

---
+++ String.py
@@ -0,0 +1,43 @@
+#  задание №1
+a = 'Hello'
+b = 'World'
+print(a, b)
+
+# задание №2
+print('HeLLo WORLd'.lower())
+
+# задание №3
+s = 'hello world'
+print(s.capitalize())
+
+# задание № 4
+s = '       hello world      '
+print(s.lstrip().rstrip())
+
+# задание №5
+s = 'h23ev9l=7lero[] fnwwqopfrfilxpd]e'
+print(s.replace('h23ev9l=7lero[] fnwwqopfrfilxpd]e', 'hello world'))
+
+# задание №6
+a = 'hello world'
+for i in a:
+    print(i)
+
+# задание №7
+s = 'HeLlOwOrLd'
+print(s.replace('HeLlOwOrLd', 'hElLoWoRlD'))
+
+# задание №8
+print('h23ev9l=7lero[] fnwwqopfrfilxpd]e'.count('e'))
+
+# задание №9
+s = 'jkds8esfie9sdfljsdl1'
+print(s.isalnum())
+
+# задание №10
+b = '92a38402830n948'
+print(s.isalnum())
+
+# задание № 11
+a = 'Hello '
+print(a * 3)
