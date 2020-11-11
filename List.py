00bc043238d697944b0668c0923ac4884752735f
DmitriyLepihin <dmitrijlepihin1991@gmail.com>
2020-11-11 11:59:13 +0300

    Work Hard

--- List.py
+++ List.py
@@ -21,3 +21,15 @@ a = [1, 2, 3, 4, 5, 6, 7]
 b = ['a', 'b', 'c']
 print(a + b)
 
+# задание № 7
+a = [1, 2, 3, 4, 5, 6, 7]
+a.reverse()
+print(a)
+
+# задание № 8
+a = [1, 2, 3, 8, 4, 5, 8, 6, 8, 7]
+cnt = 0
+for i in range(len(a)):
+    if a[i] == 8:
+        cnt += 1
+print(cnt)
