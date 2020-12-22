564fcbf89144befcc3e064e23c08d0c1e5cc6c20
DmitriyLepihin <dmitrijlepihin1991@gmail.com>
2020-12-22 21:49:40 +0300

    changes: class Account, class Shop(add: welcome_customer, info_about_product_range, checking_product_availability_in_the_store, msg_about_lack_of_money_in_the_wallet), class Customer(add balance)

--- product_shop/shop_version_two/shop_on_clasess.py
+++ product_shop/shop_version_two/shop_on_clasess.py
@@ -1,37 +1,59 @@
 from datetime import datetime
+import json
+from product_shop.shop_version_one.utils import PATH_FILE
 
 
 class Shop:
 
-    def __init__(self, accounts, customers, orders, purchase_amount=0):
+    def __init__(self, accounts, customers, orders):
         self.account = accounts
         self.customer = customers
         self.order = orders
-        self.purchase_amount = purchase_amount
+        self.purchase_amount = 0
+
+    def welcome_customer(self):
+        self.account.parse_product()
+        print('Hello, in our prooduct shop. We have:')
 
     def start(self):
-        print('Hello, in our prooduct shop. We have:')
+        self.welcome_customer()
+        self.info_about_product_range()
+        self.customer.balance()
+
+    def info_about_product_range(self):
         self.account.write_account_info(f"{datetime.today().strftime('%A %x')} \
 The following products have arrived in the store : \n", 'w')
-        for key, value in self.account.load_product_range().items():
-            print('\t'f"{key}\t{value.get('count')} kg. \ton \t{value.get('price')} byn - {value.get('type')} ")
-            self.account.write_account_info('\t'f"{key}\t{value.get('count')} kg. \ton\
-\t{value.get('price')} byn - {value.get('type')}\n", 'a')
+        for key, value in self.account.products.items():
+            print('\t'f"{key}\t{value.count} kg. \ton \t{value.price} byn - {value.p_type} ")
+            self.account.write_account_info('\t'f"{key}\t{value.count} kg. \ton\
+\t{value.price} byn - {value.p_type}\n", 'a')
 
-    def product_check(self):
-        if self.order.type_product in self.account.load_product_range():
+    def checking_product_availability_in_the_store(self):
+        if self.order.type_product in self.account.products:
             return self.order.type_product
         else:
             return False
 
+    def making_purchase(self):
+        self.account.write_account_info(f"{self.order.type_product} sold for summ = \
+{self.account.products[self.order.type_product].price * self.order.count} byn ({self.order.count} kg.)\n", 'a')
+        self.customer.buy(self.purchase_amount)
+        self.account.calculation_profit_day(self.purchase_amount)
+
     def calculation_purchase_amount(self):
-        self.purchase_amount = self.account.load_product_range()[self.order.type_product]['price'] * self.order.count
+        self.purchase_amount = self.account.products[self.order.type_product].price * self.order.count
         return self.purchase_amount
+
+    def msg_about_lack_of_money_in_the_wallet(self):
+        print(f"You haven't got enough money for buying {self.order.type_product} - {self.order.count} \
+kg.,\nyou have {self.customer.wallet} byn, you can buy no more than \
+{self.customer.wallet // self.account.products[self.order.type_product].price}\
+kg.{self.order.type_product} or choose other products!")
 
     def communication_with_customer(self):
         while True:
             if self.customer.wallet < self.account.min_price:
-                print(f"You don't have enough money. In your wallet {self.customer.wallet} byn. Sorry. Try later...")
+                print(f"You don't have enough money. Sorry. Try later...")
                 break
             self.order.create_order()
             try:
@@ -39,20 +61,12 @@ The following products have arrived in the store : \n", 'w')
             except ValueError:
                 print(f"Re-enter the product of interest, ({self.order.count}) kilograms as a number!")
                 continue
-            self.product_check()
-            if self.product_check():
+            if self.checking_product_availability_in_the_store():
                 self.calculation_purchase_amount()
                 if self.purchase_amount <= self.customer.wallet:
-                    self.account.write_account_info(f"{self.order.type_product} sold for summ = \
-{self.account.load_product_range()[self.order.type_product]['price'] * self.order.count}\
-byn ({self.order.count} kg.)\n", 'a')
-                    self.customer.credit(self.purchase_amount)
-                    self.account.calculation_profit_day(self.purchase_amount)
+                    self.making_purchase()
                 elif self.purchase_amount > self.customer.wallet > self.account.min_price:
-                    print(f"You haven't got enough money for buying {self.order.type_product} - {self.order.count} \
-kg.,\nyou have {self.customer.wallet} byn, you can buy no more than \
-{self.customer.wallet // self.account.load_product_range()[self.order.type_product]['price']}\
- kg.{self.order.type_product} or choose other products!")
+                    self.msg_about_lack_of_money_in_the_wallet()
             else:
                 print(f"Sorry {self.order.type_product} are out of stock!")
                 break
@@ -61,51 +75,24 @@ kg.,\nyou have {self.customer.wallet} byn, you can buy no more than \
 
 class Account:
 
-    def __init__(self, file_name, min_price=0, profit_day=0):
-        self.min_price = min_price
+    def __init__(self, file_name):
+        self.min_price = 0
         self.file_name = file_name
-        self.product = self.parse_product()
-        self.profit_day = profit_day
+        self.products = dict()
+        self.profit_day = 0
 
     @staticmethod
     def load_product_range():
-        return {
-            "banana": {
-                "count": 10,
-                "price": 4,
-                "type": "fruit"
-            },
-            "apple": {
-                "count": 5,
-                "price": 6,
-                "type": "fruit"
-            },
-            "orange": {
-                "count": 13,
-                "price": 4,
-                "type": "fruit"
-            },
-            "potato": {
-                "count": 47,
-                "price": 1,
-                "type": "vegetables"
-            },
-            "tomato": {
-                "count": 50,
-                "price": 4,
-                "type": "vegetables"
-            }
-        }
+        with open(PATH_FILE, 'r') as file:
+            return json.load(file)
 
     def parse_product(self):
         product = list(self.load_product_range().values())
         self.min_price = product[0]['price']
-        res = []
         for key, value in self.load_product_range().items():
             if value.get('price') < self.min_price:
                 self.min_price = value.get('price')
-            res.append(Product(key, value['type'], value['count']))
-        return res
+            self.products[key] = Product(key, value['count'], value['price'], value['type'])
 
     def write_account_info(self, text, mode):
         with open(self.file_name, mode) as file:
@@ -121,27 +108,32 @@ class Customer:
     def __init__(self, money):
         self.wallet = money
 
-    def credit(self, price):
+    def buy(self, price):
         self.wallet -= price
+        self.balance()
+
+    def balance(self):
+        print(f"You have {self.wallet} byn")
 
 
 class Product:
 
-    def __init__(self, name, p_type, count):
+    def __init__(self, name, count, price, p_type):
         self.name = name
-        self.p_type = p_type
         self.count = count
+        self.price = price
+        self.p_type = p_type
 
     def __repr__(self):
-        return f"{self.name}: count {self.count}, type {self.p_type}"
+        return f"count: {self.count}, price: {self.price}, type: {self.p_type}"
 
 
 class Order:
     order = str
 
-    def __init__(self, type_product=str, count=int):
-        self.type_product = type_product
-        self.count = count
+    def __init__(self):
+        self.type_product = str
+        self.count = int
 
     def create_order(self):
         self.order = input().lower().split()
